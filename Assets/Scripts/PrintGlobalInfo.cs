47f11596e753a0f6296dd5ca5dac042e296b43f9
Jonas <jonas.fonta@gmail.com>
2023-07-28 13:26:36 +0200

    Bg in end screen

--- Assets/Scripts/PrintGlobalInfo.cs
+++ Assets/Scripts/PrintGlobalInfo.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PrintGlobalInfo : MonoBehaviour
 {
+
+    public TextMeshProUGUI textComponent;
+
+
     // Start is called before the first frame update
     void Start()
     {
         int levelIndex = GlobalVariables.Get<int>("currentLevelIndex");
         Debug.Log(levelIndex);
+
+        textComponent.text = "Level: " + levelIndex;
     }
 }
